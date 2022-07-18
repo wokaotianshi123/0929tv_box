aad34469e3d6b6a3eecc3294fcc5523b41175731
认真の雪 <529442986@qq.com>
2022-07-18 22:41:07 +0800

    Update juzi.js

--- tvbox/juzi.js
+++ tvbox/juzi.js
@@ -36,7 +36,7 @@
 "bfshifouercijiequ": "0",
 "bfjiequqian": "空",
 "bfjiequhou": "空",
-"bfjiequshuzuqian": "class=\"stui-content__playlist",
+"bfjiequshuzuqian": "class=\"stui-content__playlis clearfix",
 "bfjiequshuzuhou": "</ul>",
 "zhuangtaiqian": "更新：</span>",
 "zhuangtaihou": "</p>",
